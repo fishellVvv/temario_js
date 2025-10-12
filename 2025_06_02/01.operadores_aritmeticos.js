a98106d0d4116d6ed40b44b0c29785d8168bc243
fishellVvv <victorvigilvela@gmail.com>
2025-10-12 13:40:32 +0200

    clase operadores

---
+++ 2025_06_02/01.operadores_aritmeticos.js
@@ -0,0 +1,30 @@
+// Asignación
+let x = 5;
+x = 1;
+
+// Suma
+x = x + 4;
+console.log(x)
+
+// Resta
+y = x - 2;
+console.log(y)
+
+// Multiplicación
+console.log(x * y)
+
+// División
+console.log(x / y)
+
+// Módulo o Resto
+console.log(x % y)
+
+// Combinados
+x += 1;
+x -= 1;
+x *= 1;
+x /= 1;
+
+// Incremento y decremento
+x++;
+x--;
---
+++ 2025_06_02/02.operadores_logicos.js
@@ -0,0 +1,31 @@
+let a = 10;
+let b = 20;
+
+// O (or)
+console.log(true || false)
+
+// Y (and)
+console.log(true && false)
+
+// No (not)
+console.log(!false)
+
+// Igual
+console.log(a == b)
+
+// Extrictamente igual
+console.log(5 === "5")
+
+// Desigual
+console.log(a != b)
+
+// Extrictamente desigual
+console.log(5 !== "5")
+
+// Mayor - Mayor o igual
+console.log(a > b)
+console.log(a >= b)
+
+// Menor - Menor o igual
+console.log(a < b)
+console.log(a <= b)
