d7d0be86a5e68dde4fc76a5e2096424e589be6d6
fishellVvv <victorvigilvela@gmail.com>
2025-10-12 19:10:54 +0200

    rename files

---
+++ 2025_06_16/08.bucles.js
@@ -0,0 +1,16 @@
+// for
+for (let i = 0; i < 5; i++) {
+    console.log("Hola", i)
+}
+
+const movies = ["John Wick", "Nosferatu", "The Evil Dead", "The Matrix"];
+for (let i = 0; i < movies.length; i++) {
+    console.log(movies[i])
+}
+
+const numbers = [5, 45, 67, 2, 78, 90, 45, 3, 2, 23, 87];
+let suma = 0;
+for (let i = 0; i < numbers.length; i++) {
+    suma += numeros[i];
+}
+console.log("La suma de números es:", suma)
